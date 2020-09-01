cbb10173774dab20809056076a14f91265872e04
git config --global user.name <stevenpcurtis@hotmail.com>
2020-09-01 16:39:53 +0100

    Initial Commit with Pods

---
+++ Pods/Target Support Files/Pods-TrackerDemo-TrackerDemoUITests/Pods-TrackerDemo-TrackerDemoUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TrackerDemo_TrackerDemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TrackerDemo_TrackerDemoUITestsVersionString[];
+
